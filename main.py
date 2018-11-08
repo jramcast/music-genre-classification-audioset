10753dc8d21355ffe5c99d8a1f4e9f083e1973eb
Jaime Ramírez <jaime.ram@gmail.com>
2018-11-08 21:11:36 +0100

    WIP: cleaning

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from music_genre.infrastructure.data import AudiosetDataLoader
 
 
 if __name__ == "__main__":
-
-    datadir = os.path.abspath(
-        "./downloads/audioset/audioset_v1_embeddings/bal_train")
+    datadir = os.environ.get(
+        "DATA_DIR",
+        "./downloads/audioset/audioset_v1_embeddings/bal_train"
+    )
+    datadir = os.path.abspath(datadir)
+    print(datadir)
 
     data_loader = AudiosetDataLoader(datadir)
     data_loader.load()
--- music_genre/infrastructure/data.py
+++ music_genre/infrastructure/data.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import tensorflow as tf
 
 
@@ -10,7 +9,7 @@ class AudiosetDataLoader:
 
     def load(self):
 
-        iterator  = self._create_data_load_graph()
+        iterator = self._create_data_load_graph()
 
         total = 0
 
@@ -27,55 +26,8 @@ class AudiosetDataLoader:
                     print(total)
                     print('----------------------------------------')
                 except tf.errors.OutOfRangeError:
+                    print('FINISH')
                     break
-
-
-        # (video_ids, labels, decoded_features) = self._create_data_load_graph()
-
-        # extracted_ids = []
-        # extracted_labels = []
-        # extracted_features = []
-
-        # # run the tensorflow session
-        # with tf.Session() as sess:
-        #     # Initialize all global and local variables
-        #     init_op = tf.group(
-        #         tf.global_variables_initializer(),
-        #         tf.local_variables_initializer()
-        #     )
-        #     sess.run(init_op)
-
-        #     # Create a coordinator and run all QueueRunner objects
-        #     # Coordinators are necessary to run queues
-        #     coord = tf.train.Coordinator()
-
-        #     threads = tf.train.start_queue_runners(coord=coord)
-        #     n = 0
-
-        #     try:
-        #         while not coord.should_stop():
-        #             vid, vlabels, vfeatures = sess.run(
-        #                 (video_ids, labels, decoded_features)
-        #             )
-        #             extracted_ids.append(vid)
-        #             extracted_labels.append(vlabels)
-        #             extracted_features.append(vfeatures)
-        #             n = n + 1
-        #             print('Done {}'.format(n))
-
-        #     except tf.errors.OutOfRangeError:
-        #         # When done, ask the threads to stop
-        #         print('Done reading')
-        #     finally:
-        #         # Stop the threads
-        #         coord.request_stop()
-
-        #     coord.join(threads)
-        #     sess.close()
-
-        # print('Num videos {}'.format(len(extracted_ids)))
-        # print('Num labels {}'.format(len(extracted_labels)))
-        # print('Num features {}'.format(len(extracted_features)))
 
     def _create_data_load_graph(self):
 
@@ -86,12 +38,11 @@ class AudiosetDataLoader:
             # tf.print(res, output_stream=sys.stdout)
             return res
 
-
         # Create a list of filenames and pass it to a queue
         dataset = tf.data.TFRecordDataset(self.filenames)
         dataset = dataset.map(self._read_record, num_parallel_calls=8)
         dataset = dataset.filter(only_10_seconds)
-        dataset = dataset.batch(4096)
+        dataset = dataset.batch(128)
         iterator = dataset.make_one_shot_iterator()
         return iterator
 
@@ -121,7 +72,6 @@ class AudiosetDataLoader:
         # NUM_CLASSES = 527
         # labels = tf.one_hot(context['labels'], NUM_CLASSES)
         return (video_id, features, labels)
-
 
     def _discover_filenames(self, datadir):
         for root, dirs, files in os.walk(datadir):
